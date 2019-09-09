1787c63a79b9e5c0188a276f7b3da014ae92705b
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2019-09-09 21:04:56 +0100

    Add Course Infos Section

--- src/components/sections/course-infos.js
+++ src/components/sections/course-infos.js
@@ -1,11 +1,176 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+// Uitls
+import colors from "../../utils/colors";
+
+/**
+ * Styled Component
+ */
+const SectionStyled = styled.section`
+	.CourseInfos--item {
+		display: flex;
+		margin-bottom: 3em;
+
+		&:nth-child(2n) {
+			flex-direction: row-reverse;
+		}
+
+		&:last-child {
+			margin-bottom: 0;
+		}
+	}
+
+	.item--content,
+	.item--picture {
+		width: 50%;
+	}
+
+	.item--content {
+		h3 {
+			color: ${colors.primary.dark};
+			text-align: center;
+		}
+	}
+
+	.item--picture {
+		margin-right: 2em;
+	}
+`;
 
 const CourseInfos = () => {
-  return (
-    <section>
-      <h1>Course Infos</h1>
-    </section>
-  )
-}
+	//
+	const pictures = useStaticQuery(graphql`
+		query {
+			Illustration1: file(
+				relativePath: { eq: "illustration-1.png" }
+			) {
+				childImageSharp {
+					fluid(maxWidth: 633) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			Illustration2: file(
+				relativePath: { eq: "illustration-2.png" }
+			) {
+				childImageSharp {
+					fluid(maxWidth: 633) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			Illustration3: file(
+				relativePath: { eq: "illustration-3.png" }
+			) {
+				childImageSharp {
+					fluid(maxWidth: 633) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+		}
+	`);
 
-export default CourseInfos
+	return (
+		<SectionStyled className="CourseInfos">
+			<div className="container">
+				{/* 1 */}
+				<article className="CourseInfos--item">
+					<div className="item--picture">
+						<Img
+							fluid={
+								pictures.Illustration1.childImageSharp
+									.fluid
+							}
+						/>
+					</div>
+
+					<div className="item--content">
+						<h3>Who is this for?</h3>
+						<p>
+							Beginner to Intermediate developers and
+							designers who want to become comfortable
+							with both JavaScript fundamentals and
+							working in the DOM without a library.
+						</p>
+						<p>
+							You should already know some JavaScript
+							to start - This isn't a JavaScript 101
+							course. We learn by application -
+							encountering many new situations while
+							talking about the hows, the whys and the
+							whats that we use to solve them.
+						</p>
+					</div>
+				</article>
+
+				{/* 2 */}
+				<article className="CourseInfos--item">
+					<div className="item--picture">
+						<Img
+							fluid={
+								pictures.Illustration2.childImageSharp
+									.fluid
+							}
+						/>
+					</div>
+
+					<div className="item--content">
+						<h3>Arbaoui Mehdi, How is this Free?</h3>
+						<p>
+							These videos are free as a thank-you to
+							everyone who has supported my premium
+							courses as well as because I believe in
+							giving back to the community. I see a
+							huge need for these videos and I really
+							think it will help many of you become
+							comfortable creating with JavaScript.
+						</p>
+						<p>
+							While I've spent over 300 hours on
+							JavaScript30, enough people find they
+							really click with my teaching style and
+							continue their learning with one of my
+							premium courses. It all works out!
+						</p>
+					</div>
+				</article>
+
+				{/* 3 */}
+				<article className="CourseInfos--item">
+					<div className="item--picture">
+						<Img
+							fluid={
+								pictures.Illustration3.childImageSharp
+									.fluid
+							}
+						/>
+					</div>
+
+					<div className="item--content">
+						<h3>Hey, I'm Arbaoui Mehdi</h3>
+						<p>
+							Beginner to Intermediate developers and
+							designers who want to become comfortable
+							with both JavaScript fundamentals and
+							working in the DOM without a library.
+						</p>
+						<p>
+							You should already know some JavaScript
+							to start - This isn't a JavaScript 101
+							course. We learn by application -
+							encountering many new situations while
+							talking about the hows, the whys and the
+							whats that we use to solve them.
+						</p>
+					</div>
+				</article>
+			</div>
+		</SectionStyled>
+	);
+};
+
+export default CourseInfos;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 // Sections
-import CourseGoal from "../components/sections/course-goal"
-import CourseInfos from "../components/sections/course-infos"
-import LessonsList from "../components/sections/lessons-list"
-import Faq from "../components/sections/faq"
+import CourseGoal from "../components/sections/course-goal";
+import CourseInfos from "../components/sections/course-infos";
+import LessonsList from "../components/sections/lessons-list";
+import Faq from "../components/sections/faq";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <CourseGoal />
-    <CourseInfos />
-    <LessonsList />
-    <Faq />
-  </Layout>
-)
+	<Layout>
+		<SEO title="Home" />
+		<CourseGoal />
+		<CourseInfos />
+		<LessonsList />
+		<Faq />
+	</Layout>
+);
 
-export default IndexPage
+export default IndexPage;
