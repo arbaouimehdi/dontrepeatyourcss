7752e1efda7946d86c2926669fb431cf43fe1df2
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2019-09-22 18:23:17 +0100

    Add the Footer Component

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,11 +1,30 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+
+// Utils
+import colors from "../../utils/colors";
+
+const StyledFooter = styled.footer`
+	padding: 0.8em 0;
+
+	background-color: ${colors.secondary.dark};
+
+	text-align: center;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 0.8em;
+	color: #fff;
+
+	p {
+		margin: 0;
+	}
+`;
 
 const Footer = () => {
-  return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
-  )
-}
+	return (
+		<StyledFooter>
+			<p>© 2019 sinj.app. All rights reserved.</p>
+		</StyledFooter>
+	);
+};
 
-export default Footer
+export default Footer;
